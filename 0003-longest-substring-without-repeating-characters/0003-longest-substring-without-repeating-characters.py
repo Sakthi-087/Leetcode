4dba98123061b747b82bc29173b72eb5dd0e9d48
Sakthi-087 <154756400+Sakthi-087@users.noreply.github.com>
2025-02-17 11:04:09 +0530

    Time: 5 ms (99.6%), Space: 17.8 MB (52.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,5 +7,6 @@ class Solution:
             if s[right] in char_index and char_index[s[right]]>=left:
                 left = char_index[s[right]]+1
             char_index[s[right]] = right
-            max_length = max(max_length, right-left+1)
+            if max_length < right-left+1:
+                max_length = right-left+1
         return max_length
