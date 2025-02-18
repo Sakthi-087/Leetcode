48ab95e7b319b2609289ab154cce385f249a1e91
Sakthi-087 <154756400+Sakthi-087@users.noreply.github.com>
2025-02-18 17:35:53 +0530

    Time: 41 ms (95.79%), Space: 28.5 MB (67.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left, right = 0, len(height)-1
-
-        largest_number = max(height)
-        largest = 0
-
-        # While it is still possible to find a greater sum
-        while (right-left)*largest_number > largest:
-            area = min(height[left], height[right]) * (right - left)
-            if area > largest:
-                largest = area
-            
-            if height[left] < height[right]:
-                left += 1
+        left,right = 0,len(height)-1
+        max_area = 0
+        mini = 0
+        while left < right:
+            if height[left]<height[right]:
+                mini = height[left]
             else:
-                right -= 1
-        
-        return largest
+                mini = height[right]
+            area = (right-left) * mini
+
+            if max_area < area:
+                max_area = area
+                
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+        return max_area
