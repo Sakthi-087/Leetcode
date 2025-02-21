25e90749e4d15b073c5fbb39a34243146bd6d935
Sakthi-087 <154756400+Sakthi-087@users.noreply.github.com>
2025-02-21 11:38:14 +0530

    Time: 23 ms (94.36%), Space: 21.8 MB (8.46%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        count=0
-        prefix_count=defaultdict(int)
-        prefix_count[0]=1
-        prefix_sum=0
-        for n in nums:
-            prefix_sum+=n
-            count+=prefix_count[prefix_sum-goal]
-            prefix_count[prefix_sum]+=1
-        return count
+        counts=defaultdict(int)
+        counts[0]=1
+        ans=curr=0
+        for i in nums:
+            curr+=i
+            ans+=counts[curr-goal]
+            counts[curr]+=1
+        return ans
